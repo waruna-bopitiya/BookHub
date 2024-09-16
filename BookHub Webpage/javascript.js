2ae67fbd80ef56bba14111e2c4b2ef7c1c5cd170
Waruna Bopitiya <161157284+waruna-bopitiya@users.noreply.github.com>
2024-09-16 17:40:26 +0530

    today

--- BookHub Webpage/javascript.js
+++ BookHub Webpage/javascript.js
@@ -19,3 +19,33 @@ function next(){
         img1.src = imageArray[0]; // Loop back to the first image
     }
 }
+<<<<<<< Updated upstream
+=======
+    
+   
+ 
+function previous(){  
+    const imageArray = [
+        "images/banners/1.jpg",
+        "images/banners/2.jpg",
+        "images/banners/3.jpg",
+        "images/banners/4.jpg"
+    ];
+
+    const img1 = document.getElementById("img1");
+    const currentImage = img1.src;
+    
+    if (currentImage.includes("images/banners/1.jpg")) {
+        img1.src = imageArray[3]; 
+    } 
+    else if (currentImage.includes("images/banners/2.jpg")) {
+        img1.src = imageArray[0]; 
+    } 
+    else if (currentImage.includes("images/banners/3.jpg")) {
+        img1.src = imageArray[1]; 
+    } 
+    else {
+        img1.src = imageArray[2 ];
+    }
+}
+>>>>>>> Stashed changes
