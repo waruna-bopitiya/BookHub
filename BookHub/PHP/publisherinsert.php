4f2c8140895799714c346eddcda3b55935c0002d
Waruna Bopitiya <161157284+waruna-bopitiya@users.noreply.github.com>
2024-09-21 04:25:04 +0530

    now

--- BookHub/PHP/publisherinsert.php
+++ BookHub/PHP/publisherinsert.php
@@ -5,6 +5,7 @@
     if(isset($_POST['submit'])) {
     $target = "images/".basename($_FILES['image']['name']);
 
+
     require 'config.php';
     
     $bookname = $_POST["bookname"];
@@ -14,26 +15,28 @@
     $publishdate = $_POST["publishdate"];
     $image = $_FILES["image"]["name"];
 
-    if(move_uploaded_file($_FILES['image']['tmp_name'], $target))
-    {
-        echo "Image uploaded successfully";
-    }
-    else
-    {
-        echo "Failed to upload image";
-    }
+    
 
-    $sql = "INSERT INTO books (bookName , authorName , price ,	category , publishdate , image) VALUES ('$bookname', '$authorname','$price','$category','$publishdate', '$image')";
-
-    if(mysqli_query($con, $sql)){
+        if(move_uploaded_file($_FILES['image']['tmp_name'], $target))
+            {
+                echo "Image uploaded successfully";
+            }
+        else
+            {
+                echo "Failed to upload image";
+            }
         
-        echo "record added succesfully to database";
-    }
-    else{
-        echo "failed to add record to database" .mysqli_error($con);
+            $sql = "INSERT INTO books (bookName , authorName , price ,	category , publishdate , image) VALUES ('$bookname', '$authorname','$price','$category','$publishdate', '$image')";
+        
+        if(mysqli_query($con, $sql)){
 
-    }
-
+                echo "record added succesfully to database";
+        }
+        else{
+                echo "failed to add record to database" .mysqli_error($con);
+            
+            }
+        
     mysqli_close($con);
 
 }
