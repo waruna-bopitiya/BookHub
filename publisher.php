2c73e0e156f4cb2e58ef1a23ec23be022201b8fb
Waruna Bopitiya <161157284+waruna-bopitiya@users.noreply.github.com>
2024-09-22 02:20:26 +0530

    q

--- publisher.php
+++ publisher.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/publisher.css">
+    <link rel="stylesheet" href="src\asserts\css\publisher.css">
     <title>Publisher | Boookhub</title>
     <script src="../jsfile/javascript.js"></script>
 </head>
@@ -14,13 +14,13 @@
         <!-- This is the main division we allocated to show as the wire frame -->
 
 
-            <div class="title_grp" style="justify-content: left;"><!-- this is the telephone number  and emailar -->
+        <div class="title_grp" style="justify-content: left;"><!-- this is the telephone number  and emailar -->
             
-                     <img src="..\images\title bar\icons8-phone-30.png" alt="TP" width="20px" height="20px" >
-                    +94764936441
-                    <img src="..\images\title bar\icons8-email-30.png" alt="Email" width="25px" height="25px" style="margin-left: 20px; margin-right: 5px;">
-                     bookhub2024@gmail.com
-             </div> 
+            <img src="src\asserts\images\title bar\icons8-phone-30.png" alt="TP" width="20px" height="20px" >
+           +94764936441
+           <img src="src\asserts\images\title bar\icons8-email-30.png" alt="Email" width="25px" height="25px" style="margin-left: 20px; margin-right: 5px;">
+            bookhub2024@gmail.com
+        </div> 
 
 
         
@@ -30,7 +30,7 @@
 
 
             <div class="title_grp" style="justify-content: right;"><!-- this is the BookHub name -->
-                 <img src="..\images\title bar\icons8-account-30.png" alt="login" width="25px" height="25px">
+                 <img src="src\asserts\images\title bar\icons8-account-30.png" alt="login" width="25px" height="25px">
                      sign in / sign up
             </div>
 
@@ -41,7 +41,7 @@
 
     <div class="menubar">
              <div class="menuimage">
-                     <img src="../images/bookhublogotransparent.png" alt="logo" width="200px" height="200px">
+                     <img src="src\asserts\images\bookhublogotransparent.png" alt="logo" width="200px" height="200px">
              </div>
             <div>
                      <h1>Publisher potral</h1>
