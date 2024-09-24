55dcd60e97f21e9d6f0ff6aec4e446c7a39ce383
Dulsandi <dulsandig@gmail.com>
2024-09-24 22:52:45 +0530

    24/09

--- category.php
+++ category.php
@@ -3,9 +3,158 @@
     require_once('bookhubheader.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src/asserts/css/category.css">
+    <title>Document</title>
+</head>
+<body>
+<div class="category_bar">
+    <div class="category-item-box">
+        <ul>
+            <li><a href="#">Fiction</a></li>
+            <li><a href="#">Children's Books</a></li>
+            <li><a href="#">Science</a></li>
+            <li><a href="#">Education</a></li>
+            <li><a href="#">Novels</a></li>
+            <li><a href="#">Short Stories</a></li>
+            <li><a href="#">Magazines</a></li>
+        </ul>
+    </div>
+</div>
+<!--Category content-->
+<div class="category_content">
+    <h5 style="color:blue; text-align:center;">Best Children's Books</h5>
+    <h2>Best Children</h2>
+    <div class="category_grid">
+        <!--Book1-->
+        <div class="book_item">
+           <img src="src\asserts\images\book1.jpg">
+           <p class="title">I wish You More</p>
+           <p class="price">
+              <span class="dis_price">LKR 800.00</span><br>
+              <span class="original_price">LKR 800.00</span>
+           </p>
+            
+           
+        </div>
+        <!--Book2-->
+        <div class="book_item">
+           <img src="src\asserts\images\book2.jpg">
+           <p class="title">In Your Own Backyard</p>
+           <p class="price">
+              <span class="dis_price">LKR 1080.00</span><br>
+              <span class="original_price">LKR 1200.00</span>
+           </p>
+            
+           
+        </div>
+        <!--Book3-->
+        <div class="book_item">
+           <img src="src\asserts\images\book3.jpg">
+           <p class="title">How many Legs</p>
+           <p class="price">
+              <span class="dis_price">LKR 595.00</span><br>
+              <span class="original_price">LKR 800.00</span>
+           </p>
+            
+           
+        </div>
+        <!--Book4-->
+        <div class="book_item">
+           <img src="src\asserts\images\book4.jpg">
+           <p class="title">Princess the Cat</p>
+           <p class="price">
+              <span class="dis_price">LKR 1000.00</span><br>
+              <span class="original_price">LKR 1100.00</span>
+           </p>
+            
+           
+        </div>
+        <!--Book5-->
+        <div class="book_item">
+           <img src="src\asserts\images\book5.jpg">
+           <p class="title">The Grizllie</p>
+           <p class="price">
+              <span class="dis_price">LKR 800.00</span><br>
+              <span class="original_price">LKR 900.00</span>
+           </p>
+            
+           
+        </div>
+    </div>
+    <!--Fiction-->
+    <h5 style="color:blue; text-align:center;">Best Fiction Books</h5>
+    <h2>Fiction</h2>
+    <div class="category_grid">
+        <!--Book1-->
+        <div class="book_item">
+           <img src="src\asserts\images\book6.jpg">
+           <p class="title">The Calculating Stars</p>
+           <p class="price">
+              <span class="dis_price">LKR 1800.00</span><br>
+              <span class="original_price">LKR 2000.00</span>
+           </p>
+            
+           
+        </div>
+        <!--Book2-->
+        <div class="book_item">
+           <img src="src\asserts\images\book7.jpg">
+           <p class="title">Snow Crash</p>
+           <p class="price">
+              <span class="dis_price">LKR 1080.00</span><br>
+              <span class="original_price">LKR 1200.00</span>
+           </p>
+            
+           
+        </div>
+        <!--Book3-->
+        <div class="book_item">
+           <img src="src\asserts\images\book8.jpg">
+           <p class="title">Fire Dance</p>
+           <p class="price">
+              <span class="dis_price">LKR 595.00</span><br>
+              <span class="original_price">LKR 800.00</span>
+           </p>
+            
+           
+        </div>
+        <!--Book4-->
+        <div class="book_item">
+           <img src="src\asserts\images\book9.jpg">
+           <p class="title">Children of Ruin</p>
+           <p class="price">
+              <span class="dis_price">LKR 1000.00</span><br>
+              <span class="original_price">LKR 1100.00</span>
+           </p>
+            
+           
+        </div>
+        <!--Book5-->
+        <div class="book_item">
+           <img src="src\asserts\images\book10.jpg">
+           <p class="title">Fire Dance</p>
+           <p class="price">
+              <span class="dis_price">LKR 800.00</span><br>
+              <span class="original_price">LKR 900.00</span>
+           </p>
+            
+           
+        </div>
+    </div>
+
+</div>
+    
+    
 
 
 
+</body>
+</html>
 
 <br>
 <br>
@@ -25,54 +174,7 @@
 <br>
 <br>
 <br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
     require_once('bookhubfooter.php');
 ?>
+
