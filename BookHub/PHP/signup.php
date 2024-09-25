dfe3e59646599ae91f20c5f2cc0d8a7d77c29b8b
umangi-i <it23537842@my.sliit.lk>
2024-09-25 12:09:51 +0530

    test

---
+++ BookHub/PHP/signup.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="src\asserts\css\login.css">
+    <title>Home page | BookHub</title>
+    <script src="src\asserts\js\login.js"></script>
+    </head>
+    <body>
+        <div class="content"  id="signup">
+            <h1 clss="ftitle">Register</h1>
+            <form method="post" action="">
+                <div class="input">
+                    <input type="text" name="fname" id="fName" value="First Name" required>
+                    <label for="fname">First Name</label>
+                </div>
+                <div class="input">
+                    <input type>
+               
