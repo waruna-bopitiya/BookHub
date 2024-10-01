f6566db1bc2666dd2a7dc4494b29b39e5621605b
Waruna Bopitiya <161157284+waruna-bopitiya@users.noreply.github.com>
2024-10-01 22:45:54 +0530

    e

---
+++ itview.php
@@ -0,0 +1,27 @@
+<?php
+include_once('bookhubheader.php');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="itemview">
+        <p>hweodvsdv</p>
+    </div>
+</body>
+</html>
+
+
+<?php
+include_once('bookhubfooter.php')
+?>
