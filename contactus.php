dfa1e713ee35062a35d9c9e025e5e159c085f12a
YasinthaLkshan <IT23826540@my.sliit.lk>
2024-10-02 18:54:58 +0530

    newnew

--- contactus.php
+++ contactus.php
@@ -8,6 +8,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="src/asserts/css/contactus.css">
 </head>
 <body>
 
@@ -20,52 +22,59 @@
 <br>
 <br>
 <br>
-<br>
-<br>
-<br>
 
-<h1>Contact US</h1>
-    
+    <header>
+            <div class="header-container">
+                <nav>
+                    <a href="#">Home</a> | <a href="#" class="active">Contact US</a>
+                </nav>
+            </div>
+    </header>
+
+
+    <div class="container">
+        <section class="contact-header">
+            <h1>Contact Us</h1>
+            <p>Bookazone Pvt Ltd is proud to present www.books.lk, the pioneer, all online bookstore in Sri Lanka. We offer the most extensive range of books for sale in Sri Lanka, sourced from THE USA, Europe to the Far East and India. A collection of old and rare works hitherto not offered on most online bookstores, are also available for the discerning bibliophile.</p>
+            <p>www.books.lk is owned and operated by Sri Lankans who are experienced e-commerce entrepreneurs and book lovers themselves. We provide a service that satisfies a long-awaited need of other book lovers in Sri Lanka and beyond: access to purchasing all kinds of books from anywhere in the island. Our commitment is to provide an excellent and dedicated service that ensures ease of ordering and payment and is complemented with prompt and free delivery of the widest range of titles to our customers.</p>
+            <p>Our online order service is simple with highly secure methods of payment, which includes an innovative, local method to enable customers who do not wish to pay by credit card, to buy books from www.books.lk. We have introduced a method by which customers can pay for the books they order online, with a physical payment at a bank.</p>
+        </section>
+
+        <section class="contact-info">
+            <div class="info-item">
+                <div class="icon">
+                    <img src="location-icon.png" alt="Location Icon">
+                </div>
+                <div class="info-text">
+                    <h2>Site</h2>
+                    <p><a href="http://www.books.lk">www.bookhub.lk</a></p>
+                </div>
+            </div>
+
+            <div class="info-item">
+                <div class="icon">
+                    <img src="phone-icon.png" alt="Phone Icon">
+                </div>
+                <div class="info-text">
+                    <h2>Contact Number</h2>
+                    <p>+94 71 110 2929</p>
+                </div>
+            </div>
+
+            <div class="info-item">
+                <div class="icon">
+                    <img src="mail-icon.png" alt="Mail Icon">
+                </div>
+                <div class="info-text">
+                    <h2>Contact via Mail</h2>
+                    <p><a href="mailto:inquiry@books.lk">bookhub@gmail.com</a></p>
+                </div>
+            </div>
+        </section>
+    </div>
 </body>
 </html>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 <br>
 <br>
 <br>
--- returnpolicy.php
+++ returnpolicy.php
@@ -4,72 +4,54 @@
 ?>
 
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="src/asserts/css/returnpolicy.css">
+</head>
+<body>
 
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+    <header>
+        <div class="header-container">
+            <nav>
+                <a href="#">Home</a> | <a href="#" class="active">Return Policy</a>
+            </nav>
+        </div>
+    </header>
 
+    <div class="content-container">
+        <h1>Returns Policy</h1>
+        <p>Returns are acceptable ONLY for defects or errors for which we are responsible, for a period up to but not exceeding 7 days from the Books.lk (our) date of shipment.</p>
 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
+        <p><strong>WARNING:</strong> <strong>DO NOT</strong> just return an item(s) without contacting us and obtaining a <strong>Return Merchandise Authorization (RMA)</strong> number! Plus we need to give you the correct address to return your item(s) to. Return freight is the customer’s responsibility. Items that are shipped back to us without following the policy will NOT be entitled to any refund. You may return an item for replacement within 7 days of the Books.lk (our) shipment date. We will take back your purchase provided the item is unused and in its original packaging.</p>
 
+        <p>*Please note that all returned merchandise must be prepaid by the purchaser, unless stated and permitted otherwise by Books.lk. Shipping and handling charges (to and from purchaser) are non-refundable and will be deducted from the refund amount before a refund or exchange credit is issued. The total refund credit amount will be the purchase order amount (or partially discounted refund of the purchase order amount, as decided by us, Books.lk the merchant, in special cases and circumstances), less shipping charges (to and from purchaser), unless stated and permitted otherwise by Books.lk (For returns where shipping charges were prepaid by the purchaser, as followed per our return policy instructions, only shipping charges to the purchaser will be deducted from the refund credit amount.)</p>
 
+        <p>You may want to insure your return, as we cannot be responsible for lost or misdirected returns. If you paid for your order by credit card, a credit (less shipping charges) will be issued and will appear on your credit card billing statement, unless an agreement is made between the merchant, Books.lk and the purchaser, for the refund credit to be issued by other means. All returns must be accompanied by the packing slip. When you return an item, follow the instructions on the packing slip, and most importantly, please indicate the reason for the return. Please contact us by phone on +94 773 205555 | +94 773 441066 (9:00 A.M. to 5:00 P.M. Mon.-Fri. Sri Lankan Standard Time) or via e-mail on the <a href="#">Contact Us</a> page as to where to return the merchandise.</p>
 
+        <p>*Please note that all shipments are inspected, checked, and well packed before leaving our warehouse. Any damage that occurs may be solely through negligence of the carrier. Visible Damage: if the shipment is delivered to you in a visibly damaged condition, insist on notation of this damage by the delivery carrier. *It is important that you notify Books.lk immediately upon noticing the damage.</p>
 
+        <p>Concealed damage: Notify Books.lk immediately upon noticing the damage. You must retain the outside carton and packing materials. Books.lk or the carrier will advise you whether to hold the damaged item(s) for a physical inspection or return the item(s).</p>
 
+        <p>In cases where a damaged merchandise report is made to Books.lk, as per our company policy and procedure, and Books.lk and/or the carrier, after return of merchandise by the customer, and an inspection of the merchandise by us or the carrier, deems that the merchandise in fact has arrived damaged to the customer, Books.lk will replace the damaged goods with an identical item. (If an identical item is no longer available, the damaged merchandise will be replaced with an item of similar value, as per mutual agreement between the customer and Books.lk.)</p>
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+        <p>If you have any questions regarding your order, please feel free to contact us at:<br>
+        Tel. +94 773 205555 | +94 773 441066 (9:00 A.M. to 5:00 P.M. Mon. – Fri. Sri Lankan Standard Time)</p>
+    </div>
+</body>
+</html>
 
 
 
